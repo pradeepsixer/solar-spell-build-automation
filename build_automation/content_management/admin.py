5f8be4afc1a77f4ad9c79cb73051178b21dc0e92
Pradeep Kumar <pradeepsixer@gmail.com>
2018-02-19 23:49:35 -0700

    SBA-37 Added Model, Views and Basic File Handling
    1. Same file cannot be uploaded twice (achieved through SHA-256 checksum)
    2. New files will replace the old files.
    3. Files will be deleted from disk once the model is deleted.

--- build_automation/content_management/admin.py
+++ build_automation/content_management/admin.py
@@ -1,3 +1,31 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from .exceptions import DuplicateContentFileException
+from .models import Content
+
+
+class ContentAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_time', 'last_updated_time',)
+    list_display = ('id', 'name', 'content_file', 'created_time', 'last_updated_time',)
+
+    def save_model(self, request, obj, form, change):
+        if 'content_file' in request.FILES:
+            obj.content_file_uploaded = True
+        try:
+            super().save_model(request, obj, form, change)
+        except DuplicateContentFileException as dup:
+            edit_url = reverse(
+                            'admin:%s_%s_change' % (obj._meta.app_label, obj._meta.model_name),
+                            args=[dup.content.pk]
+                        )
+            messages.set_level(request, messages.ERROR)
+            messages.error(
+                request,
+                mark_safe(
+                    "<strong>The uploaded file already exists \"<a href='%s'>here</a>\"</strong>" % edit_url
+                )
+            )
+
+admin.site.register(Content, ContentAdmin)
