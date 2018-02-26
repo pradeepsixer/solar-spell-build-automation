b091f3df0dbad7c2c705e430c7fe17594b68c74b
saranyadmdrn <saranyadmrn@gmail.com>
2018-02-26 00:22:34 -0700

    SBA-53 Link Tag and Contents
    Created a many-to-many relationship between Tag and Content.

--- build_automation/content_management/serializers.py
+++ build_automation/content_management/serializers.py
@@ -3,7 +3,17 @@ from rest_framework import serializers
 from .models import Content, Tag
 
 
+class TagSerializer(serializers.ModelSerializer):
+    #id = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    class Meta:
+        model = Tag
+        fields = '__all__'
+
 class ContentSerializer(serializers.HyperlinkedModelSerializer):
+    #tag = serializers.SlugRelatedField(many=True, read_only=True, slug_field='id')
+    #tag_ids = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    #tag = serializers.ModelSerializer(many=True, read_only=True)
+    tag = TagSerializer(required=False)
     def create(self, validated_data):
         content = Content(**self.validated_data)
         return self.__create_update(content, validated_data)
@@ -12,6 +22,7 @@ class ContentSerializer(serializers.HyperlinkedModelSerializer):
         content.name = validated_data.get('name', content.name)
         content.description = validated_data.get('description', content.description)
         content.content_file = validated_data.get('content_file', content.content_file)
+        content.tag = validated_data.get('tag', content.tag)
         return self.__create_update(content, validated_data)
 
     def __create_update(self, content, validated_data):
@@ -23,7 +34,7 @@ class ContentSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Content
-        fields = ('url', 'name', 'description', 'content_file', 'updated_time', 'last_uploaded_time')
+        fields = ('url', 'name', 'description', 'content_file', 'updated_time', 'last_uploaded_time', 'tag')
         extra_kwargs = {
             'url': {'lookup_field': 'pk'},
         }
