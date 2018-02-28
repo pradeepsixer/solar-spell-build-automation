eab22ae7660266067ba9254668ce4191acf96c40
Novice052 <immersed101@gmail.com>
2018-02-28 09:33:28 -0700

    SBA-86 Added updated Tag model and Admin entry

--- build_automation/content_management/admin.py
+++ build_automation/content_management/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Tag
 
-# Register your models here.
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ()  # TODO Do we need this?
+
+
+# Register the admin class with the associated model
+admin.site.register(Tag, TagAdmin)
--- build_automation/content_management/models.py
+++ build_automation/content_management/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.urls import reverse #Used to generate URLs by reversing the URL patterns
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200, null=True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True)
+
+    def get_absolute_url(self):
+
+        return reverse('tag_detail', args=[str(self.id)])
+
+    def __str__(self):
+        return self.name
+
