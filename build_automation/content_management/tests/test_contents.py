4336515017321b4495fcbcf5157eb9c4bcd6e4c8
Pradeep Kumar <pradeepsixer@gmail.com>
2018-03-01 03:31:32 -0700

    SBA-54 Added Test Cases for File Handling
    1. Duplicate File Uploading
    2. File Cleanup after Model Instance Removal

--- build_automation/content_management/tests/test_contents.py
+++ build_automation/content_management/tests/test_contents.py
@@ -8,6 +8,7 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase, override_settings
 from django.utils import timezone
 
+from content_management.exceptions import DuplicateContentFileException
 from content_management.models import Content
 
 # The directory where the temporary media will be stored in
@@ -44,6 +45,7 @@ class ContentTest(TestCase):
 
         with patch('django.utils.timezone.now', mock_timezone_now):
             content.save()
+        content.content_file.close()
 
         saved_content = Content.objects.first()
         self.assertEqual(mock_timezone_now.call_count, 1)
@@ -72,12 +74,14 @@ class ContentTest(TestCase):
 
         with patch('django.utils.timezone.now', mock_timezone_now):
             content.save()
+        content.content_file.close()
 
         new_upload_time = currtime - timedelta(days=2)
         mock_timezone_now.return_value = new_upload_time
         with patch('django.utils.timezone.now', mock_timezone_now):
             # content still has content_file_uploaded set to True
             content.save()
+        content.content_file.close()
 
         self.assertEqual(content.last_uploaded_time, new_upload_time)
 
@@ -104,6 +108,7 @@ class ContentTest(TestCase):
 
         with patch('django.utils.timezone.now', mock_timezone_now):
             content.save()
+        content.content_file.close()
 
         # mark that there is no file uploaded during the update process
         content.content_file_uploaded = False
@@ -111,5 +116,58 @@ class ContentTest(TestCase):
         mock_timezone_now.return_value = new_upload_time
         with patch('django.utils.timezone.now', mock_timezone_now):
             content.save()
+        content.content_file.close()
 
         self.assertEqual(content.last_uploaded_time, first_upload_time)
+
+    def test_duplicate_file_on_create(self):
+        """
+        Tests whether a DuplicateContentFileException is raised, when a duplicate file is uploaded,
+        when a new model instance is created
+        """
+        first_value = {
+            "name": "Content 1",
+            "description": "Content's Description",
+            "content_file": SimpleUploadedFile(
+                "uploaded_file_name", "This will be the contents of the uploaded file.".encode()
+            ),
+            "updated_time": timezone.now()
+        }
+        content1 = Content(**first_value)
+        content1.content_file_uploaded = True
+        content1.save()
+        content1.content_file.close()
+
+        second_value = {
+            "name": "Content 2",
+            "description": "Content's Description",
+            "content_file": SimpleUploadedFile(
+                "uploaded_file_name_2", "This will be the contents of the uploaded file.".encode()
+            ),
+            "updated_time": timezone.now()
+        }
+        content2 = Content(**second_value)
+        content2.content_file_uploaded = True
+        with self.assertRaises(DuplicateContentFileException) as cm:
+            content2.save()
+        self.assertEqual(cm.exception.content.pk, content1.pk)
+        content2.content_file.close()
+
+    def test_delete_content(self):
+        """
+        Tests whether the file is deleted after the model instance is deleted
+        """
+        values = {
+            "name": "Content 1",
+            "description": "Content's Description",
+            "content_file": SimpleUploadedFile(
+                "uploaded_file_name", "This will be the contents of the uploaded file.".encode()
+            ),
+            "updated_time": timezone.now()
+        }
+        content1 = Content(**values)
+        content1.content_file_uploaded = True
+        content1.save()
+        self.assertTrue(os.path.exists(content1.content_file.path))
+        content1.delete()
+        self.assertFalse(os.path.exists(content1.content_file.path))
