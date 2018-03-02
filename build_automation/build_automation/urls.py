8024a371c0485050491ef6e667669bea9959e368
Novice052 <immersed101@gmail.com>
2018-03-02 09:34:19 -0700

    SBA-62 added test view function(index())

--- build_automation/build_automation/urls.py
+++ build_automation/build_automation/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 
-from content_management.api_views import ContentApiViewset, TagViewSet
+from ..content_management.api_views import ContentApiViewset, TagViewSet
 
 router = routers.SimpleRouter()
 router.register(r'contents', ContentApiViewset)
--- build_automation/content_management/views.py
+++ build_automation/content_management/views.py
@@ -1,3 +1,13 @@
-# from django.shortcuts import render
+from django.shortcuts import render
+from .models import Tag, Content
 
-# Create your views here.
+
+def index(request):
+    num_content = Content.objects.all().count()
+    num_tags = Tag.objects.all().count()
+
+    return render(
+        request,
+        'index.html',
+        context={'num_content': num_content, 'num_tags': num_tags},
+    )
