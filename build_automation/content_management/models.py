09b23877d3aa0feca9ad293548b7b7b5cd7f3499
Pradeep Kumar <pradeepsixer@gmail.com>
2018-03-06 15:09:51 -0700

    SBA-27 Added models for Directory layout
    Added models for
     1. Directory Layout
     2. Directory
     3. Filter Criteria - for selecting contents to go into the directory.

--- build_automation/content_management/models.py
+++ build_automation/content_management/models.py
@@ -11,7 +11,7 @@ class Tag(models.Model):
         return reverse('tag-detail', args=[str(self.id)])
 
     def __str__(self):
-        return self.name
+        return "Tag[{}]".format(self.name)
 
 
 class Content(models.Model):
@@ -53,4 +53,71 @@ class Content(models.Model):
         self.original_file = self.content_file
 
     def __str__(self):
-        return self.name
+        return "Content[{}]".format(self.name)
+
+    def get_absolute_url(self):
+        return reverse('content-detail', args=[self.pk])
+
+
+class FilterCriteria(models.Model):
+    """
+    Model for specifying the filter criteria. Filter criteria is basically an expression tree, with multiple
+    criteria joined together. The leaves are the Tags.
+    """
+
+    OPERATOR_CHOICES = (
+        (1, 'AND'),
+        (2, 'OR'),
+    )
+
+    parent = models.ForeignKey('self', null=True, on_delete=models.CASCADE)
+    left_criteria = models.ForeignKey('self', related_name="left_parent", null=True, on_delete=models.SET_NULL)
+    right_criteria = models.ForeignKey('self', related_name="right_parent", null=True, on_delete=models.SET_NULL)
+    tag = models.ForeignKey(Tag, null=True, on_delete=models.CASCADE)
+    operator = models.IntegerField(null=True, choices=OPERATOR_CHOICES)
+
+    @property
+    def operator_str(self):
+        if self.operator is not None:
+            for (operator, operator_str) in self.OPERATOR_CHOICES:
+                if operator == self.operator:
+                    return operator_str
+        return None
+
+    def __str__(self):
+        if self.left_criteria is not None or self.right_criteria is not None:
+            returnstr = "{"
+            if self.left_criteria is not None:
+                returnstr += str(self.left_criteria)
+            if self.operator is not None:
+                returnstr += " " + self.operator_str + " "
+            if self.right_criteria is not None:
+                returnstr += str(self.right_criteria)
+            returnstr += "}"
+            return returnstr
+        if self.tag is not None:
+            return self.tag.name
+
+
+class DirectoryLayout(models.Model):
+    """
+    The Directory Layout for each build.
+    """
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return "DirectoryLayout[{}]".format(self.name)
+
+
+class Directory(models.Model):
+    """
+    Representation of the directory for each build.
+    """
+    name = models.CharField(max_length=50)
+    dir_layout_id = models.ForeignKey(DirectoryLayout, related_name='dir_layout', on_delete=models.CASCADE)
+    filter_criteria = models.ForeignKey(FilterCriteria, on_delete=models.SET_NULL, null=True)
+    parent = models.ForeignKey('self', related_name='subdirectories', on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return "Directory[{}]".format(self.name)
