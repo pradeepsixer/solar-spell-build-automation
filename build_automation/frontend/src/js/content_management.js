da5a153a4cabd548bed5a15c7373bcf7c8fb0483
Novice052 <immersed101@gmail.com>
2018-03-29 14:27:45 -0700

    SBA-88 adding ReactJS components (again)

---
+++ build_automation/frontend/src/js/content_management.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+class ContentManagement extends React.Component{
+    render(){
+        return <h3>Content Management</h3>
+    }
+}
+module.exports = ContentManagement
