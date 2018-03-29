d4e3ababe2ef8699a8357c21e8ca96b719c8b1e1
Pradeep Kumar <pradeepsixer@gmail.com>
2018-03-28 23:12:07 -0700

    SBA-94 Autocomplete Component with Chips
    Autocomplete component that displays multiple selections as Chips. This uses downshift package.

---
+++ build_automation/frontend/src/js/autocomplete.js
@@ -0,0 +1,216 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import Downshift from 'downshift';
+import keycode from 'keycode';
+
+import Chip from 'material-ui/Chip';
+import { MenuItem } from 'material-ui/Menu';
+import Paper from 'material-ui/Paper';
+import TextField from 'material-ui/TextField';
+import { withStyles } from 'material-ui/styles';
+
+
+const MAX_COUNT = 10;
+
+/*
+ * This file is a modified version of https://github.com/mui-org/material-ui/blob/e61ffeab3c432506829d118fb173b0dc336e224a/docs/src/pages/demos/autocomplete/IntegrationDownshift.js
+ */
+
+function renderInput(inputProps) {
+    const { InputProps, classes, ref, ...other } = inputProps;
+
+    return (
+        <TextField InputProps={{ inputRef: ref, classes: { root: classes.inputRoot, }, ...InputProps, }} {...other} />
+    );
+}
+
+function renderSuggestion({ suggestion, index, itemProps, highlightedIndex, selectedItem, searchKey }) {
+    const isHighlighted = highlightedIndex === index;
+    const isSelected = (selectedItem || '').indexOf(suggestion[searchKey]) > -1;
+
+    return (
+        <MenuItem {...itemProps} key={suggestion[searchKey]} selected={isHighlighted} component="div" style={{ fontWeight: isSelected ? 500 : 400, }}>
+            {suggestion[searchKey]}
+        </MenuItem>
+    );
+}
+
+renderSuggestion.propTypes = {
+    highlightedIndex: PropTypes.number,
+    index: PropTypes.number,
+    itemProps: PropTypes.object,
+    selectedItem: PropTypes.string,
+    suggestion: PropTypes.shape({ label: PropTypes.string }).isRequired,
+};
+
+function getSuggestions(suggestions, inputValue, maxCount, searchKey) {
+    let count = 0;
+
+    return suggestions.filter(suggestion => {
+        const keep =
+        (!inputValue || suggestion[searchKey].toLowerCase().indexOf(inputValue.toLowerCase()) !== -1) &&
+            count < maxCount;
+
+        if (keep) {
+            count += 1;
+        }
+
+        return keep;
+    });
+}
+
+class AutoCompleteWithChips extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputValue: '',
+            selectedItem: [],
+        };
+        this.handleKeyDown = this.handleKeyDown.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleDelete = this.handleDelete.bind(this);
+    }
+
+    componentWillReceiveProps(props) {
+        this.setState({
+            inputValue: '',
+            selectedItem: []
+        })
+    }
+
+    handleKeyDown(evt) {
+        const { inputValue, selectedItem } = this.state;
+        if (selectedItem.length && !inputValue.length && keycode(evt) === 'backspace') {
+            this.setState({
+                selectedItem: selectedItem.slice(0, selectedItem.length - 1),
+            });
+        }
+    };
+
+    handleInputChange(evt) {
+        this.setState({ inputValue: evt.target.value });
+    };
+
+    handleChange(item) {
+        let { selectedItem } = this.state;
+
+        if (selectedItem.indexOf(item) === -1) {
+            selectedItem = [...selectedItem, item];
+            if (this.props.onChange) {
+                var selectedSuggestion = null;
+                for (var i=0; i<this.props.suggestions.length; i++) {
+                    if(this.props.suggestions[i][this.props.searchKey] == item) {
+                        selectedSuggestion = this.props.suggestions[i];
+                    }
+                }
+                this.props.onChange(selectedSuggestion);
+            }
+        }
+
+        this.setState({
+            inputValue: '',
+            selectedItem,
+        });
+    };
+
+    handleDelete(item) {
+        const selectedItem = [...this.state.selectedItem];
+        selectedItem.splice(selectedItem.indexOf(item), 1);
+
+        this.setState({ selectedItem });
+    };
+
+    render() {
+        const { classes } = this.props;
+        const { inputValue, selectedItem } = this.state;
+
+        return (
+            <Downshift inputValue={inputValue} onChange={this.handleChange} selectedItem={selectedItem}>
+                {({
+                getInputProps,
+                getItemProps,
+                isOpen,
+                inputValue: inputValue2,
+                selectedItem: selectedItem2,
+                highlightedIndex,
+                }) => (
+                    <div className={classes.container}>
+                        {
+                            renderInput({
+                                fullWidth: true,
+                                classes,
+                                InputProps: getInputProps({
+                                startAdornment: selectedItem.map(item => (
+                                    <Chip
+                                        key={item}
+                                        tabIndex={-1}
+                                        label={item}
+                                        className={classes.chip}
+                                        onDelete={evt => this.handleDelete(item)}
+                                    />
+                                )),
+                                onChange: this.handleInputChange,
+                                onKeyDown: this.handleKeyDown,
+                                placeholder: this.props.placeholder,
+                                id: this.props.id,
+                                }),
+                            })
+                        }
+                        {
+                            isOpen ? (
+                                <Paper className={classes.paper} square>
+                                    {
+                                        getSuggestions(this.props.suggestions, inputValue2, this.props.maxCount || MAX_COUNT, this.props.searchKey).map((suggestion, index) =>
+                                            renderSuggestion({
+                                                suggestion,
+                                                index,
+                                                itemProps: getItemProps({ item: suggestion[this.props.searchKey] }),
+                                                highlightedIndex,
+                                                selectedItem: selectedItem2,
+                                                searchKey: this.props.searchKey || 'label',
+                                            }),
+                                        )
+                                    }
+                                </Paper>
+                            ) : null
+                        }
+                    </div>
+                )}
+            </Downshift>
+        );
+    }
+}
+
+AutoCompleteWithChips.propTypes = {
+    classes: PropTypes.object.isRequired,
+    suggestions: PropTypes.array,
+    onChange: PropTypes.func
+};
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    height: 250,
+  },
+  container: {
+    flexGrow: 1,
+    position: 'relative',
+  },
+  paper: {
+    position: 'absolute',
+    zIndex: 1,
+    marginTop: theme.spacing.unit,
+    left: 0,
+    right: 0,
+  },
+  chip: {
+    margin: `${theme.spacing.unit / 2}px ${theme.spacing.unit / 4}px`,
+  },
+  inputRoot: {
+    flexWrap: 'wrap',
+  },
+});
+
+export default withStyles(styles)(AutoCompleteWithChips);
