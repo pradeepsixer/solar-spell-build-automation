88fbb1cec2d073e14c2e4b2e5ba4dcbfac485380
Novice052 <immersed101@gmail.com>
2018-04-04 17:23:20 -0700

    SBA-88 management and upload UIs done (needs tweaking)

--- build_automation/frontend/src/js/content_management.js
+++ build_automation/frontend/src/js/content_management.js
@@ -7,6 +7,9 @@ import Paper from 'material-ui/Paper';
 import AppBar from 'material-ui/AppBar';
 import Typography from 'material-ui/Typography';
 import AutoCompleteWithChips from './autocomplete.js';
+import TextField from 'material-ui/TextField';
+import UploadContent from './upload_content';
+import FileListComponent from './file_list_component';
 import {APP_URLS} from "./url";
 import axios from 'axios';
 
@@ -32,17 +35,35 @@ class ContentManagement extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            tags: []
-        }
+            tags: [],
+            name: "",
+            description: "",
+            fieldErrors: {},
+            updatedTime: '',
+            files: [],
+            currentView: 'manage'
+        };
+        this.setCurrentView=this.setCurrentView.bind(this);
+        this.tagIdTagsMap= {};
     }
     componentDidMount() {
         this.loadData()
+    }
+     buildTagIdTagsMap(tags) {
+        // Builds a map of <Tag Id> - Tag
+        const tagIdTagMap = {};
+        tags.forEach(eachTag => {
+            tagIdTagMap[eachTag.id] = eachTag;
+        });
+        return tagIdTagMap;
     }
     loadData() {
         const currInstance = this;
         axios.get(APP_URLS.TAG_LIST, {
             responseType: 'json'
         }).then(function (response) {
+            currInstance.tagIdTagsMap=currInstance.buildTagIdTagsMap(response.data);
+            console.log(currInstance.tagIdTagsMap);
             currInstance.setState({
                 tags: response.data
             })
@@ -50,42 +71,50 @@ class ContentManagement extends React.Component{
             console.log(error);
             // TODO : Show the error message.
         });
+        axios.get(APP_URLS.CONTENTS_LIST, {
+            responseType: 'json'
+        }).then(function (response) {
+            currInstance.setState({
+                files: response.data
+            })
+        }).catch(function (error) {
+            console.log(error);
+            // TODO : Show the error message.
+        });
+    }
+    handleTextFieldUpdate(stateProperty, evt) {
+        const targetVal = evt.target.value;
+        this.setState((prevState, props) => {
+            const newState = {
+                fieldErrors: prevState.fieldErrors,
+                [stateProperty]: targetVal
+            };
+            newState.fieldErrors[stateProperty] = null;
+            return newState;
+        })
+    }
+    setCurrentView(viewName){
+        this.setState({currentView: viewName})
     }
     render(){
+        console.log(this.tagIdTagsMap);
     return (
         <div>
             <Grid container spacing={8} style={{paddingLeft: '20px'}}>
                     <Grid item xs={3} style={{paddingLeft: '20px'}}>
-                        <Button variant="raised" color="primary">
-                            Test
+                        <h3>Content Management</h3>
+                        <Button variant="raised" color="primary" onClick={e => {this.setCurrentView('manage')}}>
+                            Manage Content
                         </Button>
-                    <h3>Content Management</h3>
-                    <h2>Test</h2>
+                        <div style={{marginTop: '20px'}}> </div>
+                        <Button variant="raised" color="primary" onClick={e => {this.setCurrentView('upload')}}>
+                            Add Content
+                        </Button>
                     </Grid>
 
                     <Grid item xs={8}>
-                        <AppBar position="static" style={{ height: '50px', margin: 'auto'}}>
-                            <Typography gutterBottom variant="subheading" style={{color: '#ffffff'}}>
-
-                            </Typography>
-                        </AppBar>
-                        <input
-                        accept="*"
-                        className={'hidden'}
-                        id="raised-button-file"
-                        multiple
-                        type="file"
-                        />
-                    <label htmlFor="raised-button-file">
-                        <Button variant="raised" component="span">
-                        Upload
-                        </Button>
-                    </label>
-
-                        <div style={{marginTop: '20px'}}> </div>
-                        <span>
-                            <AutoCompleteWithChips suggestions={this.state.tags} searchKey={'name'} selectedItem={[]} onChange={evt => console.log(evt)}/>
-                        </span>
+                        {this.state.currentView=='manage'&&<FileListComponent allFiles={this.state.files} tagIdsTagsMap={this.tagIdTagsMap}/>}
+                        {this.state.currentView=='upload'&&<UploadContent />}
                     </Grid>
             </Grid>
 
