8f057cc02571556879340f0b6b5405d61e65d593
Sai Aditya Bodavala <bsaiaditya@gmail.com>
2018-04-14 16:33:47 -0700

    SBA-157 Added SaveCallback function
    Added save call back which takes back to tables on saving a new tag.

--- build_automation/frontend/src/js/addTag.js
+++ build_automation/frontend/src/js/addTag.js
@@ -8,17 +8,17 @@ import Grid from 'material-ui/Grid';
 import { APP_URLS, get_url } from './url.js';
 
 import Typography from 'material-ui/Typography';
+import { TAG_SAVE_TYPE } from './constants.js';
 class TagCreation extends React.Component {
     constructor(props) {
         super(props);
-        this.state={
-            currentView: 'addTag',
+        this.state = {
             name: '',
             description: ''
         };
         this.handleTextFieldUpdate = this.handleTextFieldUpdate.bind(this);
         this.saveTag = this.saveTag.bind(this);
-        this.deleteTag = this.deleteTag.bind(this); 
+        this.saveCallback = props.onSave.bind(this);
     }
 
     handleTextFieldUpdate(stateProperty, evt) {
@@ -31,85 +31,70 @@ class TagCreation extends React.Component {
         this.setState({
             name: '',
             description: ''
-            
-        });
-    }
 
-    setCurrentView(viewName, selectedPanel){
-        this.setState({
-          currentView: viewName,
-          currentpanelState: selectedPanel})
+        });
     }
 
     saveTag(evt) {
         var targetUrl = this.props.listUrl;
-        const payload = {name: this.state.name, description: this.state.description};
+        const payload = { name: this.state.name, description: this.state.description };
         const currentInstance = this;
-             {
+        {
             // Create a new Tag.
             axios.post(targetUrl, payload, {
                 responseType: 'json'
-            }).then(function(response) {
-                currentInstance.saveCallback(response.data, true);
-            }).catch(function(error) {
+            }).then(function (response) {
+                currentInstance.saveCallback(response.data, TAG_SAVE_TYPE.CREATE);
+            }).catch(function (error) {
                 console.error("Error in creating a new Tag", error);
             })
         }
     }
 
-    deleteTag(evt) {
-        const targetUrl = get_url(APP_URLS.TAG_LIST, {name:this.state.name});
-        const currentInstance = this;
-        axios.delete(targetUrl, {
-            responseType: 'json'
-        }).then(function(response) {
-            currentInstance.deleteCallback(currentInstance.state.name);
-        }).catch(function(error) {
-            console.error("Error in deleting the Tag", error);
-        })
-    }
- 
+
+
+
 
     render() {
-    return(
-        <Grid container spacing={0} style={{paddingLeft: '20px'}}>
-        <Grid item xs={8}>
-        <Button variant="raised" color="primary" onClick={e => {this.props.onCancel()}}>
-            Cancel
-            </Button>
-        <Button variant="raised" color="secondary" onClick={evt =>this.saveTag(evt)}>    
-            Save
-            </Button>
-            </Grid>
-           <Grid item xs={8}>
-           <Typography gutterBottom align="center" variant="headline" component="h2">
-           {this.props.title}
+        return (
+            <Grid container spacing={0} style={{ paddingLeft: '20px' }}>
+                <Grid item xs={8}>
+                    <Button variant="raised" color="primary" onClick={e => { this.props.onCancel() }}>
+                        Cancel
+                    </Button>
+                    <Button variant="raised" color="primary" onClick={evt => this.saveTag(evt)}>
+                        Save
+                    </Button>
+                </Grid>
+                <Grid item xs={8}>
+                    <Typography gutterBottom align="center" variant="headline" component="h2">
+                        {this.props.title}
                     </Typography>
-          </Grid>
-        <Grid item xs={8}>
-           
-            <TextField
-              id="name"
-              label="Name"
-              value={this.state.name}
-              onChange={evt => this.handleTextFieldUpdate('name', evt)}
-              fullWidth
-              margin="normal"
-            />
-            <TextField
-              id="desc"
-              label="Description"
-              value={this.state.desc}
-              onChange={evt => this.handleTextFieldUpdate('description', evt)}
-              fullWidth
-              margin="normal"
-            />
-            </Grid> 
-        </Grid>
-    );
-           
-        
-        
+                </Grid>
+                <Grid item xs={8}>
+
+                    <TextField
+                        id="name"
+                        label="Name"
+                        value={this.state.name}
+                        onChange={evt => this.handleTextFieldUpdate('name', evt)}
+                        fullWidth
+                        margin="normal"
+                    />
+                    <TextField
+                        id="desc"
+                        label="Description"
+                        value={this.state.desc}
+                        onChange={evt => this.handleTextFieldUpdate('description', evt)}
+                        fullWidth
+                        margin="normal"
+                    />
+                </Grid>
+            </Grid>
+        );
+
+
+
     }
 }
 
--- build_automation/frontend/src/js/constants.js
+++ build_automation/frontend/src/js/constants.js
@@ -4,8 +4,16 @@ const __DIRLAYOUT_SAVE_TYPE = {
     CLONE: 'clone'
 }
 
+const __TAG_SAVE_TYPE = {
+    CREATE: 'create',
+    UPDATE: 'update'
+}
+
+
 const DIRLAYOUT_SAVE_TYPE = Object.freeze(__DIRLAYOUT_SAVE_TYPE);
+const TAG_SAVE_TYPE = Object.freeze(__TAG_SAVE_TYPE);
 
 module.exports = {
-    DIRLAYOUT_SAVE_TYPE
+    DIRLAYOUT_SAVE_TYPE,
+    TAG_SAVE_TYPE
 }
